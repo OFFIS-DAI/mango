07667d4f307db74848559db587bb951eac326d5d
marvin <mnw@offis.de>
2021-05-18 13:09:35 +0200

    success on sending is tested for now

---
+++ mango/tests/unit_tests/test_agent.py
@@ -0,0 +1,127 @@
+import pytest
+import asyncio
+from typing import Dict, Any
+from mango.core.container import Container
+from mango.core.agent import Agent
+
+
+class PingPongAgent(Agent):
+    """
+    Simple PingPongAgent that can send ping and receive pong messages
+    """
+    def __init__(self, container):
+        super().__init__(container)
+        self.open_ping_requests = {}
+        self.sending_tasks = []
+
+    def handle_msg(self, content, meta: Dict[str, Any]):
+        # answer on ping
+        if content == 'ping':
+            assert 'sender_addr' in meta.keys() and 'sender_id' in meta.keys()
+
+            # get addr and id from sender
+            receiver_host, receiver_port = meta['sender_addr']
+            receiver_id = meta['sender_id']
+            # send back pong, providing your own details
+            t = asyncio.create_task(self._container.send_message(
+                content='pong', receiver_addr=(receiver_host, receiver_port), receiver_id=receiver_id,
+                acl_metadata={'sender_addr': self._container.addr,
+                              'sender_id': self._aid},
+                create_acl=True)
+            )
+            self.sending_tasks.append(t)
+
+        elif content == 'pong':
+            assert 'sender_addr' in meta.keys() and 'sender_id' in meta.keys()
+            # get host, port and id from sender
+            sender_host, sender_port = meta['sender_addr']
+            sender_id = meta['sender_id']
+            assert ((sender_host, sender_port), sender_id) in self.open_ping_requests.keys()
+
+            self.open_ping_requests[((sender_host, sender_port), sender_id)].set_result(True)
+
+    async def send_ping_to_other(self, other_addr, other_id):
+        # create
+        self.open_ping_requests[(other_addr, other_id)] = asyncio.Future()
+        success = await self._container.send_message(
+            content='ping', receiver_addr=other_addr, receiver_id=other_id,
+            acl_metadata={'sender_addr': self._container.addr, 'sender_id': self._aid},
+            create_acl=True)
+        assert success
+
+    async def wait_for_sending_messages(self, timeout=1):
+        for t in self.sending_tasks:
+            if not t.done():
+                try:
+                    await asyncio.wait_for(t, timeout=timeout)
+                except asyncio.TimeoutError:
+                    assert False, 'Timeout occurred while waiting for sending a message'
+
+            assert t.exception() is None and t.result(), 'Sending of at least one message failed'
+
+    async def wait_for_pong_replies(self, timeout=1):
+        for addr_tuple, fut in self.open_ping_requests.items():
+            try:
+                await asyncio.wait_for(fut, timeout=timeout)
+            except asyncio.TimeoutError:
+                print('Timeout occurred while waiting for the ping response of %s, '
+                      'going to check if all messages could be send' % str(addr_tuple))
+                await self.wait_for_sending_messages()
+                assert False, 'Not all pong replies have arrived on time'
+
+
+@pytest.mark.asyncio
+async def test_init_and_shutdown():
+    c = await Container.factory(addr=('127.0.0.1', 5555))
+    a = PingPongAgent(c)
+    assert a.aid is not None
+    assert not a._check_inbox_task.done()
+    assert not c._check_inbox_task.done()
+    await a.shutdown()
+    await c.shutdown()
+    assert a.stopped
+    assert not c.running
+    assert len(asyncio.all_tasks()) == 1
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("num_agents,num_containers",
+                         [(1, 1), (2, 1), (2, 2), (10, 2), (10, 10)])
+async def test_send_ping_pong(num_agents, num_containers):
+    # create containers
+    containers = []
+    for i in range(num_containers):
+        c = await Container.factory(addr=('127.0.0.2', 5555 + i))
+        containers.append(c)
+
+    # create agents
+    agents = []
+    addrs = []
+    for i in range(num_agents):
+        c = containers[i % num_containers]
+        a = PingPongAgent(c)
+        agents.append(a)
+        addrs.append((c.addr, a._aid))
+
+    # all agents send ping request to all agents (including themselves)
+    for a in agents:
+        for receiver_addr, receiver_id in addrs:
+            await a.send_ping_to_other(other_addr=receiver_addr, other_id=receiver_id)
+
+    for a in agents:
+        if a._check_inbox_task.done():
+            if a._check_inbox_task.exception() is not None:
+                raise a._check_inbox_task.exception()
+            else:
+                assert False, f'check_inbox terminated unexpectedly.'
+    for a in agents:
+        # await a.wait_for_sending_messages()
+        await a.wait_for_pong_replies()
+
+    # gracefully shutdown
+    for a in agents:
+        await a.shutdown()
+    for c in containers:
+        await c.shutdown()
+
+    assert len(asyncio.all_tasks()) == 1
