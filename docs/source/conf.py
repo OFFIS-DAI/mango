04d8c63eac56042d28eea844145f27b7ea21d0c8
Rico Schrage <rico.schrage@uni-oldenburg.de>
2021-09-14 12:14:08 +0200

    Add mango to sys path for autodocs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,11 @@ import os
 import sys
 import sphinx_rtd_theme
 
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, os.path.abspath('../../mango'))
+
 # -- Project information -----------------------------------------------------
 
 project = 'mango'
