cc9927a9647a753890b9f4fd5cff004fe8294d4d
Rico Schrage <rico.schrage@uni-oldenburg.de>
2021-11-10 16:52:49 +0100

    Test for Agent facadde

---
+++ tests/unit_tests/core/test_agent_schedule.py
@@ -0,0 +1,31 @@
+
+from typing import Any, Dict
+
+import pytest, asyncio
+
+from mango.core.agent import Agent
+from mango.core.container import Container
+
+class MyAgent(Agent): 
+    def handle_msg(self, content, meta: Dict[str, Any]):
+        pass
+
+
+@pytest.mark.asyncio
+async def test_periodic_facade():
+    # GIVEN        
+    c = await Container.factory(addr=('127.0.0.2', 5555))
+    agent = MyAgent(c)
+    l = []
+    async def increase_counter():
+        l.append(1)
+
+    # WHEN
+    t = agent.schedule_periodic_task(increase_counter, 2)
+    try: 
+        await asyncio.wait_for(t, timeout=3)
+    except asyncio.exceptions.TimeoutError:
+        pass
+
+    # THEN
+    assert len(l) == 2
