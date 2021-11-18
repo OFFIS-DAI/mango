afabc65cc384da6ed974f796d0dba7a9bc21b1c6
Rico Schrage <rico.schrage@uni-oldenburg.de>
2021-11-18 12:19:28 +0000

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ DESCRIPTION = 'Modular Python Agent Framework'
 EMAIL = 'mango@offis.de'
 AUTHOR = 'Mango Team'
 REQUIRES_PYTHON = '>=3.7.0'
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
