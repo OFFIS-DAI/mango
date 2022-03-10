ad07d23d06afc9d38fb3557da0a61b08cf43693c
Jens Sager <jens.sager@offis.de>
2022-03-10 12:43:19 +0100

    Add test case for simple mqtt and tcp message passing Ref:#9

---
+++ tests/integration_tests/test_message_roundtrip.py
@@ -0,0 +1,167 @@
+import pytest
+from mango.core.agent import Agent
+from mango.core.container import Container
+from mango.messages.codecs import JSON, PROTOBUF
+import asyncio
+
+M1 = "Hello"
+M2 = "Hello2"
+M3 = "Goodbye"
+
+
+async def setup_and_run_test_case(connection_type, codec):
+    comm_topic = "test_topic"
+    init_addr = ("localhost", 1555) if connection_type == "tcp" else None
+    repl_addr = ("localhost", 1556) if connection_type == "tcp" else None
+
+    broker = ("localhost", 1883, 60)
+    mqtt_kwargs_1 = {
+        "client_id": "container_1",
+        "broker_addr": broker,
+        "transport": "tcp",
+    }
+
+    mqtt_kwargs_2 = {
+        "client_id": "container_2",
+        "broker_addr": broker,
+        "transport": "tcp",
+    }
+
+    container_1 = await Container.factory(
+        connection_type=connection_type,
+        codec=codec,
+        addr=init_addr,
+        mqtt_kwargs=mqtt_kwargs_1,
+    )
+    container_2 = await Container.factory(
+        connection_type=connection_type,
+        codec=codec,
+        addr=repl_addr,
+        mqtt_kwargs=mqtt_kwargs_2,
+    )
+
+    if connection_type == "mqtt":
+        init_target = repl_target = comm_topic
+    else:
+        init_target = repl_addr
+        repl_target = init_addr
+
+    init_agent = InitiatorAgent(container_1, init_target)
+    repl_agent = ReplierAgent(container_2, repl_target)
+
+    repl_agent.other_aid = init_agent._aid
+    init_agent.other_aid = repl_agent._aid
+
+    await asyncio.gather(repl_agent.start(), init_agent.start())
+    await asyncio.gather(
+        container_1.shutdown(),
+        container_2.shutdown(),
+    )
+
+
+# InitiatorAgent:
+# - send "Hello"
+# - awaits reply
+# - answers to reply
+# - shuts down
+class InitiatorAgent(Agent):
+    def __init__(self, container, target):
+        super().__init__(container)
+        self.target = target
+        self.other_aid = None
+
+        self.got_reply = asyncio.Future()
+
+    def handle_msg(self, content, meta):
+        if content == M2:
+            self.got_reply.set_result(True)
+
+    async def start(self):
+        if getattr(self._container, "subscribe_for_agent", None):
+            await self._container.subscribe_for_agent(aid=self.aid, topic=self.target)
+
+        await asyncio.sleep(0.1)
+
+        # send initial message
+        await self._container.send_message(
+            M1,
+            self.target,
+            receiver_id=self.other_aid,
+            create_acl=True,
+        )
+
+        # await reply
+        await self.got_reply
+
+        # answer to reply
+        await self._container.send_message(
+            M3,
+            self.target,
+            receiver_id=self.other_aid,
+            create_acl=True,
+        )
+
+        # shut down
+        pass
+
+
+# ReplierAgent:
+# - awaits "Hello"
+# - sends reply
+# - awaits reply
+# - shuts down
+class ReplierAgent(Agent):
+    def __init__(self, container, target):
+        super().__init__(container)
+        self.target = target
+        self.other_aid = None
+
+        self.got_first = asyncio.Future()
+        self.got_second = asyncio.Future()
+
+    def handle_msg(self, content, meta):
+        if content == M1:
+            self.got_first.set_result(True)
+        elif content == M3:
+            self.got_second.set_result(True)
+
+    async def start(self):
+        if getattr(self._container, "subscribe_for_agent", None):
+            await self._container.subscribe_for_agent(aid=self.aid, topic=self.target)
+
+        # await "Hello"
+        await self.got_first
+
+        # send reply
+        await self._container.send_message(
+            M2,
+            self.target,
+            receiver_id=self.other_aid,
+            create_acl=True,
+        )
+
+        # await reply
+        await self.got_second
+
+        # shut down
+        pass
+
+
+@pytest.mark.asyncio
+async def test_tcp_json():
+    await setup_and_run_test_case("tcp", JSON())
+
+
+@pytest.mark.asyncio
+async def test_tcp_proto():
+    await setup_and_run_test_case("tcp", PROTOBUF())
+
+
+@pytest.mark.asyncio
+async def test_mqtt_json():
+    await setup_and_run_test_case("mqtt", JSON())
+
+
+@pytest.mark.asyncio
+async def test_mqtt_proto():
+    await setup_and_run_test_case("mqtt", PROTOBUF())
