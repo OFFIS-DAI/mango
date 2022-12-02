fe8c2bb2c88c0753a9f3b2292f58be4904b83444
Rico Schrage <rico.schrage@uni-oldenburg.de>
2022-12-02 15:56:47 +0100

    Remove mango as namespace package.

--- mango/__init__.py
+++ mango/__init__.py
@@ -1,6 +1,6 @@
-from mango.container.factory import create as create_container
-from mango.agent.core import Agent
-from mango.role.core import RoleAgent
-from mango.role.api import Role, RoleContext
+from .container.factory import create as create_container
+from .agent.core import Agent
+from .role.core import RoleAgent
+from .role.api import Role, RoleContext
 
 
