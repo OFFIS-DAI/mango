c4018501d36f5164b3d5725328a33aaf7e4692e4
Rico Schrage <rico.schrage@uni-oldenburg.de>
2022-12-13 18:07:13 +0100

    Moved Role API to agent.role.

--- mango/__init__.py
+++ mango/__init__.py
@@ -1,6 +1,5 @@
 from .container.factory import create as create_container
 from .agent.core import Agent
-from .role.core import RoleAgent
-from .role.api import Role, RoleContext
+from .agent.role import RoleAgent, Role, RoleContext
 
 
