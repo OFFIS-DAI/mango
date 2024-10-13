b6043aca9419fb32fbe467ac015fe85d2fda6ce4
Rico Schrage <rico.schrage@uni-oldenburg.de>
2024-10-13 16:38:42 +0200

    Up to 3.10

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ DESCRIPTION = "Modular Python Agent Framework"
 URL = "https://github.com/OFFIS-DAI/mango"
 EMAIL = "mango@offis.de"
 AUTHOR = "mango Team"
-REQUIRES_PYTHON = ">=3.9.0"
+REQUIRES_PYTHON = ">=3.10.0"
 VERSION = "2.0.0"
 
 # What packages are required for this module to be executed?
